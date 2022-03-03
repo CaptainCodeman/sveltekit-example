052162ca10b74c298b5d744ed8cb826aa68354ed
Simon Green <simon@captaincodeman.com>
2022-03-03 12:27:45 -0700

    Cleanup svelte.config

--- svelte.config.js
+++ svelte.config.js
@@ -1,28 +1,17 @@
-import fs from 'fs'
 import preprocess from 'svelte-preprocess'
-import adapt from '@sveltejs/adapter-node'
-
-const firebase = JSON.parse(fs.readFileSync('node_modules/firebase/package.json', 'utf8'))
+import adapt from '@sveltejs/adapter-static'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
+
 	kit: {
 		// By default, `npm run build` will create a standard Node app.
 		// You can create optimized builds for different platforms by
 		// specifying a different adapter
 		adapter: adapt(),
-
-		vite: {
-			build: {
-				sourcemap: true,
-			},
-			define: {
-				FIREBASE_SDK_VERSION: JSON.stringify(firebase.version),
-			}
-		}
 	}
 }
 
