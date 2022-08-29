78477536f2f01d76224869dc9766d6a951980deb
Simon Green <simon@captaincodeman.com>
2022-08-29 11:23:58 -0600

    don't use `invalidate()` to update LayoutData just override session on client when it's been updated
    fixes #17

--- src/lib/session.ts
+++ src/lib/session.ts
@@ -1,12 +1,14 @@
-import { derived } from 'svelte/store'
+import { derived, writable } from 'svelte/store'
 import { browser } from '$app/environment'
-import { invalidate } from '$app/navigation'
 import { page } from '$app/stores'
-import { dedupe } from './dedupe'
 import { auth } from './auth'
 
+// internal store allows us to override the page data session without having to invalidate LayoutData
+const internal = writable()
+
 // derived store from page data to provide our session
-export const session = dedupe(derived(page, $page => $page.data.session))
+export const external = derived(page, $page => $page.data.session)
+export const session = derived([internal, external], ([$internal, $external]) => $internal || $external)
 
 // if we're using session, we need to keep the server-side auth-state in sync with the client
 // this subscribes to the firebase client-side auth state and posts changes back to the server
@@ -29,12 +31,11 @@ async function syncSession() {
           : fetch('/session', {
             method: 'delete'
           })
-        const res = await req
 
         // TODO: handle errors if session can't be set on server ...
-
-        // invalidate the session to re-populate the session store
-        await invalidate()
+        const res = await req
+        const data = await res.json()
+        internal.set(data)
       })
     }
   })
--- src/routes/session/+server.ts
+++ src/routes/session/+server.ts
@@ -2,6 +2,7 @@ import { serialize } from 'cookie'
 import { dev } from '$app/environment'
 import type { RequestHandler } from './$types'
 import { auth } from '$lib/admin'
+import { json } from '@sveltejs/kit'
 
 const WEEK_IN_SECONDS = 60 * 60 * 24 * 7
 const WEEK_IN_MILLISECONDS = WEEK_IN_SECONDS * 1000
@@ -10,11 +11,12 @@ const WEEK_IN_MILLISECONDS = WEEK_IN_SECONDS * 1000
 export const POST: RequestHandler = async ({ request }) => {
   const token = await request.text()
 
+  const user = await auth.verifyIdToken(token)
   const sessionCookie = await auth.createSessionCookie(token, { expiresIn: WEEK_IN_MILLISECONDS })
   const options = { maxAge: WEEK_IN_SECONDS, httpOnly: true, secure: !dev }
   const cookie = serialize('session', sessionCookie, options)
 
-  return new Response(null, { headers: { 'Set-Cookie': cookie } })
+  return json({ user }, { headers: { 'Set-Cookie': cookie } })
 }
 
 // DELETE clears the session cookie
@@ -22,5 +24,5 @@ export const DELETE: RequestHandler = async ({ }) => {
   const options = { expires: new Date(0), httpOnly: true, secure: !dev }
   const cookie = serialize('session', '', options)
 
-  return new Response(null, { headers: { 'Set-Cookie': cookie } })
+  return json({ user: null }, { headers: { 'Set-Cookie': cookie } })
 }
