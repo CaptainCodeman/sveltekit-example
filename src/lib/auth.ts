8223985e9293843efc98aacecc0083a6078c5867
Fausto Davila <diukescd@gmail.com>
2022-10-18 22:53:03 +0200

    Corrected spelling mistake

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -4,7 +4,7 @@ import type { Auth } from "firebase/auth"
 import type { FirebaseApp } from 'firebase/app'
 import { app } from './app'
 
-// load the firebase auth client as a store and provide an API to access it's methods
+// load the firebase auth client as a store and provide an API to access its methods
 // this depends on the app store and will also only be loaded on demand
 // so no firebase JS loaded unless the page needs it
 const createAuth = () => {
