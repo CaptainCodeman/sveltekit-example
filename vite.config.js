8fa62844818c21ea992c6f25d700f07b2fbc2813
Simon Green <simon@captaincodeman.com>
2022-12-14 15:20:22 -0700

    update to SvelteKit 1.0.0

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,10 @@
+import { defineConfig } from 'vite'
 import { sveltekit } from '@sveltejs/kit/vite'
-import dns from 'dns'
 
-dns.setDefaultResultOrder('verbatim')
-
-/** @type {import('vite').UserConfig} */
-const config = {
+export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
 		host: 'localhost',
 		port: 3000,
 	},
-}
-
-export default config
+})
